8f9cfaa802a1cd6e1e6e84dce2e8602c58cc0ebb
David Tonge <contact@davidtonge.me>
2019-04-17 22:44:42 -0400

    merge conflicts

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,11 +5,10 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <div className='container'>
-        <div className='content has-text-centered'>
+        <div className='content'>
           <p>
             {config.copyright}
           </p>
-          <p>Powered by <a href='https://www.gatsbyjs.org'>Gatsby</a> and <a href='https://www.netlifycms.org'>Netlify CMS</a> | <a href='https://github.com/v4iv/gatsby-starter-business'>Github Repository</a></p>
         </div>
       </div>
     </footer>
