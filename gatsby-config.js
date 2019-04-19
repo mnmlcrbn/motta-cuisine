07c4eee86712b60ec97163483d5dc224f21246e5
David Tonge <contact@davidtonge.me>
2019-04-19 19:54:32 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,7 @@ module.exports = {
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
               maxWidth: 2048,
+              pathPrefix: '/',
             },
           },
         ],
