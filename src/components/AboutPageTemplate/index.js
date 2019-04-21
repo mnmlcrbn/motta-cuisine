769a22ee63135f13a2ed30c5f8ddc25abb8d8b3b
David Tonge <contact@davidtonge.me>
2019-04-21 16:46:05 -0400

    horiz flip

--- src/components/AboutPageTemplate/index.js
+++ src/components/AboutPageTemplate/index.js
@@ -21,15 +21,14 @@ const AboutPageTemplate = ({
         <h1 className='mc-heading'>{title}</h1>
         <div className='columns'>
           <div className='column is-6'>
-            <img src={about_main} />
-            <p className='mc-callout'>{about_callout}</p>
-          </div>
-          <div className='column is-5 is-offset-1'>
             <h3 className='mc-secondary__heading'>{secondary_heading}</h3>
             <p className='mc-secondary__copy'>In the Spring of 2014 Motta Cuisine was launched in the heart of Spanish Harlem with the help of fellow Crucians. From its launch MC Executive Chef Ralph Motta has been the driving force behind many innovative culinary experiences. Many of which began with his 3 course brunches once offered through Feastly. Within its first 2 years of operation MC was servicing the NYC, PEN & STX markets</p>
             <p className='mc-secondary__copy'>In the Fall of 2016 MC closed its doors for 6 months while Chef Motta mourned the loss of his father Robert W. Motta.</p>
             <p className='mc-secondary__copy'>Recently relocating home to St. Croix Chef Motta relaunched MC in the Spring of 2017. The brands connection to the history and culture of the islands made the transition effortless. Chef Motta's style can be described as a fusion of Caribbean, French and American cuisine.</p>
             <div className='columns'>
+              <div className='column is-4'>
+                <img src={ralph} />
+              </div>
               <div className='column is-6'>
                 <h3 className='mc-secondary__heading'>Ralph Motta</h3>
                 <ul>
@@ -38,10 +37,11 @@ const AboutPageTemplate = ({
                   <li>Creative Director</li>
                 </ul>
               </div>
-              <div className='column is-6'>
-                <img src={ralph} />
-              </div>
             </div>
+          </div>
+          <div className='column is-5 is-offset-1'>
+            <img src={about_main} />
+            <p className='mc-callout'>{about_callout}</p>
           </div>
         </div>
       </div>
