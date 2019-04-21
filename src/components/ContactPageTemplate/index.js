5c44a12f713db7fc49555124ad00781816b2d068
David Tonge <contact@davidtonge.me>
2019-04-21 15:12:01 -0400

    Form Check

--- src/components/ContactPageTemplate/index.js
+++ src/components/ContactPageTemplate/index.js
@@ -18,28 +18,29 @@ class ContactPageTemplate extends Component {
         </Helmet>
         <section className='section'>
           <form
-            name="contact"
-            method="post"
-            action="/success"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
+            name='contact'
+            method='post'
+            action='/success'
+            data-netlify='true'
+            data-netlify-honeypot='bot-field'
           >
-            <input type="hidden" name="bot-field" />
+            <input type='hidden' name='bot-field' />
+            <input type='hidden' name='form-name' value='contact' />
             <div>
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
+              <label htmlFor='name'>Name</label>
+              <input type='text' name='name' id='name' />
             </div>
             <div>
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <label htmlFor='email'>Email</label>
+              <input type='text' name='email' id='email' />
             </div>
             <div>
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="6" required />
+              <label htmlFor='message'>Message</label>
+              <textarea name='message' id='message' rows='6' required />
             </div>
             <div>
-              <input type="submit" value="Drop a line" />
-              <input type="reset" value="Eraser" />
+              <input type='submit' value='Drop a line' />
+              <input type='reset' value='Eraser' />
             </div>
           </form>
         </section>
